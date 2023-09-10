983bc4dc5ab64ac9cc5bbce2ee62ab28061558af
Sagar Dutta <duttasagar086@gmail.com>
2023-09-10 12:07:11 +0530

    Updates

--- routes/admin.js
+++ routes/admin.js
@@ -1,90 +1,122 @@
 const express = require("express");
 const router = express.Router();
-const con = require("../server");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mysql = require('mysql2');
+const secret = "jaishriram"
 
+// Create a new database connection
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "duttasagar086",
+  database: "cricbuzz"
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected to the database!');
+});
+
+// Signup Route
 router.post("/signup", (req, res) => {
-  //fetching data from req body
+  // Fetching data from req body
   const { username, password, email } = req.body;
-  //encrypting password
-  password = bcrypt.hashSync(password, 10);
+
+  // Encrypting password
+  const hashedPassword = bcrypt.hashSync(password, 10); // Hash the password
+
+  // Set isAdmin to true
   const isAdmin = true;
+
+  // SQL query to insert a new admin
   const insertQuery = `
-  INSERT INTO Admin (username, password, email, isAdmin)
-  VALUES (${username}, ${password}, ${email}, ${isAdmin})`;
+    INSERT INTO Admin (username, password, email, isAdmin)
+    VALUES (?, ?, ?, ?)`;
 
-  con.query(insertQuery, (err, result) => {
-    if (err) throw err;
-    console.log("Data inserted successfully.");
-  });
-  
-  const usernameToFetch = username;
-
-  // Query to fetch the id based on the username
-  const selectIdQuery = "SELECT id FROM Admin WHERE username = ?";
-
-  con.query(selectIdQuery, [usernameToFetch], (err, results) => {
-    if (err) throw err;
-
-    if (results.length > 0) {
-      const adminId = results[0].id;
-      res.json({
-        message: "Admin Account successfully created",
-        status: 200,
-        user_id: adminId,
-      });
-    } else {
-      console.log(`No admin found with the username ${usernameToFetch}`);
+  con.query(insertQuery, [username, hashedPassword, email, isAdmin], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error creating admin account" });
     }
+
+    console.log("Admin account created successfully.");
+    
+    // Query to fetch the id based on the username
+    const selectIdQuery = "SELECT id FROM Admin WHERE username = ?";
+
+    con.query(selectIdQuery, [username], (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Error fetching admin ID" });
+      }
+
+      if (results.length > 0) {
+        const adminId = results[0].id;
+        return res.status(200).json({
+          message: "Admin Account successfully created",
+          status_code: 200,
+          user_id: adminId,
+        });
+      } else {
+        console.log(`No admin found with the username ${username}`);
+        return res.status(404).json({ error: "Admin not found" });
+      }
+    });
   });
 });
 
+// Login Route
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
-  const usernameToSearch = username;
 
+  // SQL query to retrieve admin data by username
   const selectQuery = `
-  SELECT id, password, isAdmin
-  FROM Admin
-  WHERE username = ?
-`;
+    SELECT id, password, isAdmin
+    FROM Admin
+    WHERE username = ?`;
 
-  con.query(selectQuery, [usernameToSearch], (err, results) => {
-    if (err) throw err;
+  con.query(selectQuery, [username], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error fetching admin data" });
+    }
 
     if (results.length === 0) {
-      res.json({
+      return res.status(401).json({
         status: "Incorrect username/password provided. Please retry",
         status_code: 401,
       });
-    } else {
-      const adminRecord = results[0];
-      const adminId = adminRecord.id;
-      const isAdmin = adminData.isAdmin;
-      const adminPassword = adminRecord.password;
+    }
 
-      if (bcrypt.compareSync(password, adminPassword)) {
-        const token = jwt.sign(
-          {
-            isAdmin: isAdmin,
-          },
-          secret,
-          { expiresIn: "1d" }
-        );
-        res.json({
-          status: "Login successful",
-          status_code: 200,
-          user_id: adminId,
-          access_token: token,
-        });
-      } else {
-        res.json({
-          status: "Incorrect username/password provided. Please retry",
-          status_code: 401,
-        });
-      }
+    const adminRecord = results[0];
+    const adminId = adminRecord.id;
+    const isAdmin = adminRecord.isAdmin;
+    const adminPassword = adminRecord.password;
+
+    if (bcrypt.compareSync(password, adminPassword)) {
+      // Generate a JWT token
+      const token = jwt.sign(
+        {
+          isAdmin: isAdmin,
+        },
+        secret, 
+        { expiresIn: "1d" }
+      );
+
+      return res.status(200).json({
+        status: "Login successful",
+        status_code: 200,
+        user_id: adminId,
+        access_token: token,
+      });
+    } else {
+      return res.status(401).json({
+        status: "Incorrect username/password provided. Please retry",
+        status_code: 401,
+      });
     }
   });
 });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,36 +1,23 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql2');
 const morgan = require('morgan');
 require('dotenv/config');
 const authJwt = require('./helpers/jwt');
 const errorHandler = require('./helpers/error-handler');
 
-//middleware
+// Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
 app.use(errorHandler);
 
-//Database
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "duttasagar086",
-    database: "elio"
-  });
-  
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log('Connected!');
-  });
+// Server
+app.listen(3000, () => {
+  console.log('server is running http://localhost:3000');
+});
 
-//Server
-app.listen(3000, ()=>{
-    console.log('server is running http://localhost:3000');
-})
+// Routes
+app.use(`/api/admin`, require('./routes/admin'));
 
-//Route
-app.use(`/api/admin`, require('./routes/admin'))
-
-module.exports = con;
+// Export the 'app' object
+module.exports = app;
